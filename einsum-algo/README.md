be795034d7c287d5679321cf272405d48c3ac60d
JoelMathewC <joelmathewcherian@gmail.com>
2025-09-21 11:07:24 -0400

    prims completed

--- einsum-algo/README.md
+++ einsum-algo/README.md
@@ -1,2 +1,12 @@
 # Inferences from writing graph algos in Einsum
 
+1. Language Specification
+    - Implement a few more graph algorithms to finalize on the spec for the language
+    - The spec should clarify the grammar of the language
+    - GLAM should compile down to use finch code (and not einsum)
+    - The GLAM implementation might benefit from having pre-allocated output buffers instead of having that generated (as is the case with einsum)
+      - This would be useful because `@einsum d[i] <<min>>= k[i]` will not actually do the min of `d[i]` and `k[i]` since the output vector is re-allocated initialized to `initial_value`
+2. Optimization possibilities
+    - Read more about active set optimizations and try to find algorithms (floyd warshall might) that benefit from it
+    - Look through existing literature to identify other optimizations that are shared across graph algorithms. How do we identify that a certain optimization is applicable?
+    - Should GLAM support advanced datastructures like queues to implement certain algorithms like prims more easily?
--- einsum-algo/prims.jl
+++ einsum-algo/prims.jl
@@ -1,43 +1,47 @@
 using Finch
 
-function prims_einsum(adj_matrix,src)
+function prims_einsum(adj_matrix)
+    mst_weight = 0
+
     G = Tensor(Dense(SparseList(Element(10^8))), adj_matrix)
     (n, _) = size(G)
 
-    # Initialization for mst
-    mst = Tensor(Dense(SparseList(Element(0))), n, n)
-    @finch mst[src,src] = 0
-
-    # Initializations to calculate pending of each node
-    pending = Tensor(Dense(SparseList(Element(0))), 1, n)
-
-    # Declare the starting state of the frontier
-    frontier = Tensor(Dense(SparseList(Element(0))), 1, n)
-    @finch frontier[1,src] = 1
-    @finch pending[1,src] = 10^8
-
-    fnz_count = Scalar(1)
-
-    # Loop till the frontier is no longer empty
-    for t in 1:n
-        @einsum c[k,i] <<minby>>= ((frontier[k,i] * G[i,j] * pending[k,j]) => (i,j))
-        
-        (edge_src, edge_dst) = c[1,src][2]
-        @finch mst[edge_src,edge_dst] = 1
-        @finch pending[1,edge_dst] = 10^8
-
-        frontier = Tensor(Dense(SparseList(Element(0))), 1, n)
-        @finch frontier[1,edge_dst] = 1
-        src = edge_dst
+    min_edge = Scalar(Inf => (0=>0))
+    @finch for j in 1:n
+        for i in 1:n
+            min_edge[] <<minby>>= G[i,j] => (i => j)
+        end
     end
 
-    return mst
+    src = min_edge[][2][1]
+
+    edge_set = Tensor(SparseByteMap(Element(0)), n)
+    @finch edge_set[src] = 10^8
+
+    d = Tensor(SparseByteMap(Element(10^8)), G[src,:])
+
+    for t in 1:(n-1)
+        min_edge = Scalar(Inf => 0)
+        @finch for i in 1:n
+            min_edge[] <<minby>>= (d[i] + edge_set[i]) => i
+        end
+
+        mst_weight += min_edge[][1]
+        dst = min_edge[][2]
+        @finch edge_set[dst] = 10^8
+
+        @finch for i in 1:n
+            d[i] <<min>>= G[dst,i]
+        end
+    end
+
+    return mst_weight
 end
 
 adj_matrix = [   
-        0    1    1; 
-        0    0    0; 
-        0    1    0
+        0       1       1; 
+        10^8    0    10^8; 
+        10^8    1       0
     ]
 
-print(prims_einsum(adj_matrix,1))
+print(prims_einsum(adj_matrix))
